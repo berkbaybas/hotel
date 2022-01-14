167a6f6b61b9a5a132c920fecfb3db6eb4d8df79
berk <berk_baybas@hotmail.com>
2022-01-15 00:02:53 +0300

    hero section added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 import './styles/main.scss'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import heroBackground from '../../assets/hero_background.png'
+
+function Hero() {
+  return (
+    <main className="Hero">
+      <img src={heroBackground} alt="" />
+      <div className="Hero-discover">
+        <div className="Hero-discover-title">
+          <h1>
+            Welcome the <span>Bougainvillea</span> Hotel
+          </h1>
+        </div>
+        <p>
+          This quote best describes how we would want our guests to feel. Though
+          we are nestled in the heart of Bougainvillea.
+        </p>
+        <button>Discover More</button>
+      </div>
+    </main>
+  )
+}
+
+export default Hero
